5b5e1548ce94a8f8dfe1b9878ee1017532a1662d
hogesako <hogesako@ikasako.com>
2021-01-03 12:28:48 +0900

    initial commit

---
+++ src/face_recognition_svm_execute.py
@@ -0,0 +1,22 @@
+import face_recognition
+from sklearn import svm
+import os
+import pickle
+
+with open('model.pickle', mode='rb') as fp:
+    clf = pickle.load(fp)
+
+# Load the test image with unknown faces into a numpy array
+test_image = face_recognition.load_image_file('test_image.jpg')
+
+# Find all the faces in the test image using the default HOG-based model
+face_locations = face_recognition.face_locations(test_image)
+no = len(face_locations)
+print("Number of faces detected: ", no)
+
+# Predict all the faces in the test image using the trained classifier
+print("Found:")
+for i in range(no):
+    test_image_enc = face_recognition.face_encodings(test_image)[i]
+    name = clf.predict([test_image_enc])
+    print(*name)
