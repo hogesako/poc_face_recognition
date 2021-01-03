eb6f45f204d3b95bb4f618c1d8f849a654850d3b
hogesako <hogesako@ikasako.com>
2021-01-03 09:31:40 +0000

    add devide

--- src/face_recognition_svm_draw_rect.py
+++ src/face_recognition_svm_draw_rect.py
@@ -17,17 +17,21 @@ for person_img in test_dirs:
     pil_image = Image.fromarray(test_image)
     draw = ImageDraw.Draw(pil_image)
 
+    print(person_img)
     face_locations = face_recognition.face_locations(test_image, number_of_times_to_upsample=2)
     if len(face_locations) == 0:
-        print(person_img)
         face_locations = face_recognition.face_locations(test_image, number_of_times_to_upsample=0, model="cnn")
+    print(person_img)
     gc.collect()
     face_encodings = face_recognition.face_encodings(test_image, face_locations)
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         np_name = clf.predict([face_encoding])
         name = np_name.tolist()[0]
-        pil_image.crop((left - 10, top - 10, right + 10, bottom + 10)).save('devided/'+ name + '/' + person_img , quality=95)
-
+        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+        text_width, text_height = draw.textsize(name)
+        draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+    pil_image.save("test_out/"+ "outed_" + person_img)
     del draw
     del pil_image
     gc.collect()
--- src/face_recognition_svm_train.py
+++ src/face_recognition_svm_train.py
@@ -2,6 +2,7 @@ import face_recognition
 from sklearn import svm
 import pickle
 import os
+import gc
 
 # Training the SVC classifier
 encodings = []
@@ -22,17 +23,20 @@ for person in train_dir:
 
         face_bounding_boxes = face_recognition.face_locations(face, number_of_times_to_upsample=2)
         if len(face_bounding_boxes) == 0:
+            gc.collect()
             face_bounding_boxes = face_recognition.face_locations(face, number_of_times_to_upsample=0, model="cnn")
 
         #If training image contains exactly one face
         if len(face_bounding_boxes) == 1:
-            face_enc = face_recognition.face_encodings(face)[0]
+            face_enc = face_recognition.face_encodings(face, face_bounding_boxes)[0]
             # Add face encoding for current image with corresponding label (name) to the training data
             encodings.append(face_enc)
             names.append(person)
-            print(person)
         else:
             print(person + "/" + person_img + " was skipped and can't be used for training")
+
+        del face
+        gc.collect()
 
 # Create and train the SVC classifier
 clf = svm.SVC(gamma='scale')
